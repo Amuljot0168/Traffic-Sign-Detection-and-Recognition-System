c5d1e2631f06e26654dea07f052a3761b2d96363
Tamanna <tamanna6044.be21@chitkara.edu.in>
2025-05-22 15:30:25 +0530

    updated tuning parameters and set frame_skip in config

--- config/config.h
+++ config/config.h
@@ -6,6 +6,8 @@ const std::string YOLO_MODEL_PATH = "../models/yolov5s_detection.onnx";
 const std::string CNN_MODEL_PATH  = "../models/cnn_recognition.onnx";
 
 // Tunable hyperparameters
-const float CONF_THRESHOLD = 0.40;
-const float NMS_THRESHOLD = 0.45;
+const float CONF_THRESHOLD = 0.30;   // 40
+const float NMS_THRESHOLD = 0.45;    // 45 try switching this around
+
+const int SKIP_FRAMES = 5;
 
